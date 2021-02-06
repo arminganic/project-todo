518c5af407ab65fedc17f6d3911e389f9ca77da3
Armin Ganic <armin.ganic@gmail.com>
2021-02-06 22:46:18 +0100

    added dotenv as dependency and extracted port

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
 import todoDatabase from "./database/index.js";
 import addTodoController from "./controllers/add-todo.js";
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
-
-const database = todoDatabase;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   const response = addTodoController(req);
@@ -14,5 +15,5 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-  database.insert();
+  todoDatabase.insert();
 });

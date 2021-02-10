bf3ad8dd69355dd4d49e885a571f707ad0db7387
Armin Ganic <armin.ganic@gmail.com>
2021-02-10 18:32:21 +0100

    changed message on startup

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.get(`${apiRoot}/todos`, makeCallback(todoController.getAll));
 app.post(`${apiRoot}/todo`, makeCallback(todoController.create));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
 
 function makeCallback(controllerFunction) {
